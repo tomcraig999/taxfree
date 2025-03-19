0bc546f3e771d3d3202640e9fd2b7490c207a764
tomcraig999 <tomcraig999@gmail.com>
2025-03-19 13:02:52 +0000

    Update configuration for new repository

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 
 # Build and export the Next.js app
 echo "Building and exporting the Next.js app..."
-NEXT_PUBLIC_BASE_PATH=/offshore-portal npm run build
+NEXT_PUBLIC_BASE_PATH=/taxfree npm run build
 
 # Remove old files from gh-pages directory
 echo "Removing old files from gh-pages directory..."
@@ -10,8 +10,8 @@ rm -rf gh-pages/*
 
 # Copy the exported files to the gh-pages directory
 echo "Copying exported files to gh-pages directory..."
-mkdir -p gh-pages/offshore-portal
-BASE_URL="/offshore-portal"
+mkdir -p gh-pages/taxfree
+BASE_URL="/taxfree"
 cp -R out/* gh-pages${BASE_URL}/
 
 echo "Deployment files prepared in the gh-pages directory."
